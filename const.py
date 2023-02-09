9540cdabb3b1a97e390fcd1061e1dbdb90622102
Yizhou CHEN <yizhou.chen@sjtu.edu.cn>
2023-02-09 17:32:18 +0800

    update constants

--- const.py
+++ const.py
@@ -1,6 +1,11 @@
 import numpy as np
 import enum
 
+
+# tooth existence of the patient
+TOOTH_EXIST_MASK = {"0": np.ones((28,), np.bool_), "1": np.ones((28,), np.bool_)} 
+
+
 # Mask used to project the contour of the selected teeth in photos of different views
 MASK_UPPER = np.array([ True,  True,  True,  True,  True,  True,  True,   
                   True,  True,  True,  True,  True,  True,  True,  
@@ -41,5 +46,11 @@ class PHOTO(enum.Enum):
 NUM_PC = 10 
 NUM_POINT = 1500
 PG_SHAPE = (NUM_POINT, 3)
+
+# FDI TOOTH NUMEBRING
 UPPER_INDICES = [11,12,13,14,15,16,17,21,22,23,24,25,26,27] # ignore wisdom teeth
 LOWER_INDICES = [31,32,33,34,35,36,37,41,42,43,44,45,46,47] # ignore wisdom teeth
+
+# Teeth boundary segmentation
+IMG_SHAPE = (512, 512, 3)
+LBL_SHAPE = IMG_SHAPE[:2]
